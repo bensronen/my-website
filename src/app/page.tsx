f40f3b145d6a911f18d96e9419c42a8b953e3c3b
Ben Ronen <bsr9379@nyu.edu>
2026-01-31 16:21:55 -0500

    Fix homepage syntax and background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <main className="bg-oklch(21% 0.006 285.885) dark:text-white min-h-screen flex flex-col items-center justify-center">
-      <h1>Ben Ronen</h1>
+    <main className="bg-[oklch(21%_0.006_285.885)] text-white min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold">Ben Ronen</h1>
       <p>Fourth year studying Computer Science and Philosophy at NYU</p>
-      <p> NYC / Bay Area</p>
+      <p>NYC / Bay Area</p>
     </main>
   )
 }
