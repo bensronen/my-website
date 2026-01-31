3895d00917d8abc04f8c97745ebceed8339adc11
Ben Ronen <bsr9379@nyu.edu>
2026-01-31 15:33:49 -0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="bg-oklch(21% 0.006 285.885) dark:text-white min-h-screen flex flex-col items-center justify-center">
+      <h1>Ben Ronen</h1>
+      <p>Fourth year studying Computer Science and Philosophy at NYU</p>
+      <p> NYC / Bay Area</p>
+    </main>
+  )
+}
