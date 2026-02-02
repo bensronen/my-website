e2214947721bea866565ccc550d3f17c351fe865
Ben Ronen <bsr9379@nyu.edu>
2026-02-02 16:03:06 -0500

    fixed for mobile, hopefully

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[#FBFAF2] text-black flex items-center justify-center px-6 gap-12">
-      <img src="/ben_img_4_website.JPG" alt="Ben Ronen" className="w-128 rounded-lg" />
-      <div className="text-left max-w-xl leading-[1.6]">
+    <main className="min-h-screen bg-[#FBFAF2] text-black flex flex-col md:flex-row items-center justify-center px-6 gap-12">
+      <img src="/ben_img_4_website.JPG" alt="Ben Ronen" className="w-64 md:w-128 rounded-lg" />
+      <div className="text-center md:text-left max-w-xl leading-[1.6]">
         <h1 className="text-3xl font-bold">Ben Ronen</h1>
         <p>Fourth year studying Computer Science and Philosophy at NYU</p>
         <p>NYC / Bay Area</p>
