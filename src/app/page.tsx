1a86c6e6bac69a44b2e896f6f97dd5128c1defa8
Ben Ronen <bsr9379@nyu.edu>
2026-02-02 13:35:57 -0500

    changed font to satoshi and background to off white

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
-    <main className="bg-[oklch(21%_0.006_285.885)] text-white min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold">Ben Ronen</h1>
-      <p>Fourth year studying Computer Science and Philosophy at NYU</p>
-      <p>NYC / Bay Area</p>
+    <main className="min-h-screen bg-[#FBFAF2] text-black flex items-center justify-center px-6">
+      <div className="text-center max-w-xl">
+        <h1 className="text-3xl font-bold">Ben Ronen</h1>
+        <p>Fourth year studying Computer Science and Philosophy at NYU</p>
+        <p>NYC / Bay Area</p>
+      </div>
     </main>
   )
 }
