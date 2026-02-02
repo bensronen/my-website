e51863a811dda8be6165009aad67f350a7569893
Ben Ronen <bsr9379@nyu.edu>
2026-02-02 15:17:44 -0500

    added profile photo and 2 column layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[#FBFAF2] text-black flex items-center justify-center px-6">
-      <div className="text-center max-w-xl">
+    <main className="min-h-screen bg-[#FBFAF2] text-black flex items-center justify-center px-6 gap-12">
+      <img src="/ben_img_4_website.JPG" alt="Ben Ronen" className="w-128 rounded-lg" />
+      <div className="text-left max-w-xl leading-[1.6]">
         <h1 className="text-3xl font-bold">Ben Ronen</h1>
         <p>Fourth year studying Computer Science and Philosophy at NYU</p>
         <p>NYC / Bay Area</p>
